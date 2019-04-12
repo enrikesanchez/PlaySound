28cb3f0c70dfc03e190c2620560399c7507c720d
user148189 <user148189@74-80-245-25.ic2net.net>
2019-04-11 23:08:32 -0700

    First Draft

--- PlaySound/ViewController.swift
+++ PlaySound/ViewController.swift
@@ -2,19 +2,36 @@
 //  ViewController.swift
 //  PlaySound
 //
-//  Created by user148189 on 4/11/19.
+//  Created by esanchez on 4/11/19.
 //  Copyright © 2019 Tec. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var audioPlayer : AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func playButtonPressed(_ sender: UIButton) {
+        if let soundURL = Bundle.main.url(forResource: "AudioFile", withExtension: "mp3") {
+            
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+            }
+            catch {
+                print(error)
+            }
+            
+            audioPlayer.play()
+        }else{
+            print("Unable to locate audio file")
+        }
+    }
 }
 
